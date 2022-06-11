9171987ab78637982a29d0dc2166f9f99db4720b
Eivind Dalholt <eividalh@stud.ntnu.no>
2022-06-11 23:27:44 +0200

    member endpoints working

--- functions/index.js
+++ functions/index.js
@@ -6,6 +6,7 @@ admin.initializeApp();
 
 const app = express();
 
+// Get all group data
 app.get("/:id", async (req, res) => {
   const snapshot = await admin.firestore()
       .collection("group").doc(req.params.id).get();
@@ -16,14 +17,24 @@ app.get("/:id", async (req, res) => {
   res.status(200).send(JSON.stringify({id: groupId, ...groupData}));
 });
 
+// Create new group
 app.post("/", async (req, res) => {
-  const group = req.body;
+  const body = req.body;
 
-  await admin.firestore().collection("group").add(group);
+  const group = {
+    groupName: body.groupName,
+    members: {},
+    icon: false,
+    penaltySize: 50,
+  };
 
-  res.status(201).send();
+  await admin.firestore().collection("group").add(group)
+      .then(function(docRef) {
+        res.status(201).send(JSON.stringify({id: docRef.id}));
+      });
 });
 
+// Update data in collection
 app.put("/:id", async (req, res) => {
   const body = req.body;
 
@@ -32,6 +43,65 @@ app.put("/:id", async (req, res) => {
   res.status(200).send();
 });
 
+// New group member
+app.post("/:id/members", async (req, res) => {
+  const body = req.body;
+  const snapshot = await admin.firestore()
+      .collection("group").doc(req.params.id).get();
+
+  const members = {
+    members: {
+      ...snapshot.data().members,
+      [body.name]: 0,
+    }};
+
+  await admin.firestore().collection("group").doc(req.params.id)
+      .update(members);
+
+  res.status(200).send();
+});
+
+// Remove group member
+app.delete("/:id/members", async (req, res) => {
+  const body = req.body;
+
+  const snapshot = await admin.firestore()
+      .collection("group").doc(req.params.id).get();
+
+  const members = {
+    members: {
+      ...snapshot.data().members,
+    },
+  };
+  delete members.members[body.name];
+
+  await admin.firestore().collection("group").doc(req.params.id)
+      .update(members);
+
+  res.status(200).send();
+});
+
+// Increase/Decrease amount for person
+app.post("/:id/penalty", async (req, res) => {
+  const body = req.body;
+
+  const snapshot = await admin.firestore()
+      .collection("group").doc(req.params.id).get();
+
+  const members = {
+    members: {
+      ...snapshot.data().members,
+    },
+  };
+  members.members[body.name] += body.amount;
+
+  await admin.firestore().collection("group").doc(req.params.id)
+      .update(members);
+
+  res.status(200).send();
+});
+
+// Delete group
 app.delete("/:id", async (req, res) => {
   await admin.firestore().collection("group").doc(req.params.id).delete();
 
@@ -39,10 +109,3 @@ app.delete("/:id", async (req, res) => {
 });
 
 exports.group = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   response.send("Hello from Firebase!");
-// });
